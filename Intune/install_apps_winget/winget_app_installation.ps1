49dd73074ad9fe1816aeb41dbd81051da769917c
Michael <michael.frank@devoteam.com>
2025-05-15 23:03:21 +0200

    Delete Intune/install_apps_winget/winget_app_installation.ps1

--- Intune/install_apps_winget/winget_app_installation.ps1
+++
@@ -1,8 +0,0 @@
-$ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe\winget.exe"
-    if ($ResolveWingetPath){
-           $WingetPath = $ResolveWingetPath[-1].Path
-    }
-
-$Wingetpath = Split-Path -Path $WingetPath -Parent
-cd $wingetpath
-.\winget.exe install --exact --id Obsidian.Obsidian --silent --accept-package-agreements --accept-source-agreements --scope machine
