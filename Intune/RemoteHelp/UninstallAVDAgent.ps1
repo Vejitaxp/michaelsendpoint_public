451a54f7d19c8d13fd2f1980255930c5b654e2a9
Michael <michael.frank@devoteam.com>
2026-09-23 00:01:40 +0200

    Refactor UninstallAVDAgent.ps1 for clarity and structure

--- Intune/RemoteHelp/UninstallAVDAgent.ps1
+++ Intune/RemoteHelp/UninstallAVDAgent.ps1
@@ -1,62 +1,87 @@
-    $Location = "C:\AVDDownload"
-    $links = (Invoke-WebRequest -Uri "https://learn.microsoft.com/en-gb/intune/remote-help/deploy?tabs=windows#configure-remote-help-apps").Links                                                                  
-    $AVD = $links | where-object {$_.outerHTML -like "*Azure Virtual Desktop*"} | Get-Unique | Select-Object href                                
-    $BOOT = $links | where-object {$_.outerHTML -like "*Azure Virtual Desktop Agent Bootloader*"} | Get-Unique | Select-Object href
+<#PSScriptInfo
+    .VERSION
+        1.0.0
+    .AUTHOR
+        Michael Frank
+    .COMPANYNAME
+        michaelsendpoint.com
+    .Name
+        UninstallAVDAgent.ps1
+    .SYNOPSIS
+        Download and uninstall the Azure Virtual Desktop Agent & Azure Virtual Desktop Agent Bootloader.
+    .creationdate
+        22.09.2026
+    .lasteditdate
+        22.09.2026
+#>
 
-    New-Item -type Directory $Location
-    Set-Location $Location
+# ------------------------------------------------- Parameter --------------------------------------------------------------
 
-    $files = @(
-        @{
-            Uri = $AVD.href
-            OutFile = 'AzureVirtualDesktopAgent.msi'
-        },
-        @{
-            Uri = $BOOT.href
-            OutFile = 'AzureVirtualDesktopAgentBootloader.msi'
-        }
-    )
+$Location = "C:\AVDDownload"
+$links = (Invoke-WebRequest -Uri "https://learn.microsoft.com/en-gb/intune/remote-help/deploy?tabs=windows#configure-remote-help-apps").Links                                                                  
+$AVD = $links | where-object {$_.outerHTML -like "*Azure Virtual Desktop*"} | Get-Unique | Select-Object href                                
+$BOOT = $links | where-object {$_.outerHTML -like "*Azure Virtual Desktop Agent Bootloader*"} | Get-Unique | Select-Object href
 
-    $jobs = @()
+# ------------------------------------------------- Download ---------------------------------------------------------------
 
-    foreach ($file in $files) {
-        $jobs += Start-ThreadJob -Name $file.OutFile -ScriptBlock {
-            $params = $Using:file
-            Invoke-WebRequest @params
-        }
+New-Item -type Directory $Location
+Set-Location $Location
+
+$files = @(
+    @{
+        Uri = $AVD.href
+        OutFile = 'AzureVirtualDesktopAgent.msi'
+    },
+    @{
+        Uri = $BOOT.href
+        OutFile = 'AzureVirtualDesktopAgentBootloader.msi'
     }
+)
 
-    Write-Host "Downloads started..."
-    Wait-Job -Job $jobs
+$jobs = @()
 
-    foreach ($job in $jobs) {
-        Receive-Job -Job $job
+foreach ($file in $files) {
+    $jobs += Start-ThreadJob -Name $file.OutFile -ScriptBlock {
+        $params = $Using:file
+        Invoke-WebRequest @params
     }
+}
 
-    Write-Host "Downloads finished"
+Write-Host "Downloads started..."
+Wait-Job -Job $jobs
 
-    $files = Get-ChildItem -Path .\*.msi
+foreach ($job in $jobs) {
+    Receive-Job -Job $job
+}
 
-    Write-Host "Uninstall started..."
+Write-Host "Downloads finished"
 
-    Foreach ($file in $files) {
-      $DataStamp = get-date -Format yyyyMMddTHHmmss
-      $logFile = '{0}-{1}.log' -f $file.fullname,$DataStamp
-      $MSIArguments = @(
-          "/x"
-          ('"{0}"' -f $file.fullname)
-          "/qn"
-          "/norestart"
-          "/L*v"
-          $logFile
-      )
-      Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow 
-    }
+# ------------------------------------------------- Uninstallation ----------------------------------------------------------
 
-    Write-Host "Uninstall finished"
+$files = Get-ChildItem -Path .\*.msi
 
-    Move-Item -Path .\*.log -Destination "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs"
+Write-Host "Uninstall started..."
 
-    Set-Location "C:\"
+Foreach ($file in $files) {
+  $DataStamp = get-date -Format yyyyMMddTHHmmss
+  $logFile = '{0}-{1}.log' -f $file.fullname,$DataStamp
+  $MSIArguments = @(
+      "/x"
+      ('"{0}"' -f $file.fullname)
+      "/qn"
+      "/norestart"
+      "/L*v"
+      $logFile
+  )
+  Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow 
+}
 
-    Remove-Item $Location -recurse
+Write-Host "Uninstall finished"
+
+# ------------------------------------------------- Move logs --------------------------------------------------------------
+
+Move-Item -Path .\*.log -Destination "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs"
+
+Set-Location "C:\"
+
+Remove-Item $Location -recurse
